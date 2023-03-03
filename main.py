0b93932b323a5e522394ec37bac1bf9cb9a9a73e
Ibtehal.Moahmed <Ibtehal.Moahmed@syngenta.com>
2023-03-02 19:12:00 -0500

    done

---
+++ main.py
@@ -0,0 +1,39 @@
+import streamlit as st
+import pandas as p
+
+
+st.header("The Best Company")
+content = """
+Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod 
+tempor incididunt ut labore et dolore magna aliqua. 
+Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut 
+aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit 
+in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+Excepteur sint occaecat cupidatat non proident, sunt in culpa qui 
+officia deserunt mollit anim id est laborum."""
+
+st.write(content)
+
+st.subheader("Our Team")
+
+col1,col2,col3 = st.columns(3)
+
+df = p.read_csv('data.csv')
+
+with col1:
+    for index, row in df[:4].iterrows():
+        st.subheader(f"{row['first name'].title()} {row['last name'].title()}")
+        st.write(row['role'])
+        st.image("images/"+row['image'])
+
+with col2:
+    for index, row in df[4:8].iterrows():
+        st.subheader(f"{row['first name'].title()} {row['last name'].title()}")
+        st.write(row['role'])
+        st.image("images/"+row['image'])
+
+with col3:
+    for index, row in df[8:12].iterrows():
+        st.subheader(f"{row['first name'].title()} {row['last name'].title()}")
+        st.write(row['role'])
+        st.image("images/"+row['image'])
